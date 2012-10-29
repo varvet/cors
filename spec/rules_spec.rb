a97c7ff46f14439d23da0a94173766ee85ebdf8b
Kim Burgestrand <kim@burgestrand.se>
2012-10-29 13:48:22 +0100

    Clear out all ruby warnings in the test suite

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -2,47 +2,42 @@ describe CORS::Rules do
   describe "#each" do
     let(:list) { [] }
     let(:rules) do
-      CORS::Rules.new do |rules|
-        list << rules.required("yay", //)
-        list << rules.optional("boo", //)
+      CORS::Rules.new do |r|
+        list << r.required("yay", //)
+        list << r.optional("boo", //)
       end
     end
 
     it "is enumerable" do
-      result = rules.each_with_object([]) { |rule, result| result << rule }
-
-      result.should eq(list)
+      rules.each_with_object([]) { |rule, result| result << rule }.should eq(list)
     end
 
     it "returns an enumerator without a block" do
-      result = rules.each.with_object([]) { |rule, result| result << rule }
-
-      result.should eq(list)
+      rules.each.with_object([]) { |rule, result| result << rule }.should eq(list)
     end
   end
 
   describe "#required" do
     it "does not accept arbitrary constraints" do
-      rules = lambda { |rules| rules.required "method", false }
-      expect { CORS::Rules.new(&rules) }.to raise_error(ArgumentError, /unknown matcher/)
+      expect { CORS::Rules.new { |r| r.required "method", false } }.to raise_error(ArgumentError, /unknown matcher/)
     end
 
     it "results in an error when the value is missing" do
-      rules  = CORS::Rules.new { |rules| rules.required "method", // }
+      rules  = CORS::Rules.new { |r| r.required "method", // }
       errors = rules.validate({})
 
       errors.should eq({ "method" => [:required, rules.first] })
     end
 
     it "results in an error when the value does not match" do
-      rules = CORS::Rules.new { |rules| rules.required "content-type", %r|image/jpe?g| }
+      rules = CORS::Rules.new { |r| r.required "content-type", %r|image/jpe?g| }
       errors = rules.validate({ "content-type" => "image/png" })
 
       errors.should eq({ "content-type" => [:match, rules.first] })
     end
 
     it "can match a regexp" do
-      rules = CORS::Rules.new { |rules| rules.required "content-type", %r|image/jpe?g| }
+      rules = CORS::Rules.new { |r| r.required "content-type", %r|image/jpe?g| }
 
       rules.validate({ "content-type" => "image/jpeg" }).should be_empty
       rules.validate({ "content-type" => "image/jpg" }).should be_empty
@@ -50,14 +45,14 @@ describe CORS::Rules do
     end
 
     it "can match a literal string" do
-      rules = CORS::Rules.new { |rules| rules.required "content-type", "image/jpeg" }
+      rules = CORS::Rules.new { |r| r.required "content-type", "image/jpeg" }
 
       rules.validate({ "content-type" => "image/jpeg" }).should be_empty
       rules.validate({ "content-type" => "image/jpg" }).should_not be_empty
     end
 
     it "can match an array" do
-      rules = CORS::Rules.new { |rules| rules.required "content-type", ["image/jpeg", "image/png"] }
+      rules = CORS::Rules.new { |r| r.required "content-type", ["image/jpeg", "image/png"] }
 
       rules.validate({ "content-type" => "image/jpeg" }).should be_empty
       rules.validate({ "content-type" => "image/png" }).should be_empty
@@ -65,12 +60,11 @@ describe CORS::Rules do
     end
 
     it "can match a block" do
-      rules = lambda do |rules|
-        rules.required "content-type" do |type|
+      rules = CORS::Rules.new do |r|
+        r.required "content-type" do |type|
           "image/jpeg" == type
         end
       end
-      rules = CORS::Rules.new(&rules)
 
       rules.validate({ "content-type" => "image/jpeg" }).should be_empty
       rules.validate({ "content-type" => "image/png" }).should_not be_empty
@@ -79,14 +73,14 @@ describe CORS::Rules do
 
   describe "#optional" do
     it "results in no error when the value is missing" do
-      rules = CORS::Rules.new { |rules| rules.optional "method", // }
+      rules = CORS::Rules.new { |r| r.optional "method", // }
       errors = rules.validate({})
 
       errors.should be_empty
     end
 
     it "results in an error when the value is present but does not match" do
-      rules = CORS::Rules.new { |rules| rules.optional "content-type", %r|image/jpe?g| }
+      rules = CORS::Rules.new { |r| r.optional "content-type", %r|image/jpe?g| }
       errors = rules.validate({ "content-type" => "image/png" })
 
       errors.should eq({ "content-type" => [:match, rules.first] })
